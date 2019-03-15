9346edadb41d564f5ecdd0b963ab63aa888f6f38
GaoKai <gkaigk1987@gmail.com>
2019-03-15 20:57:32 +0800

    添加了Zuul重试功能
    添加了Zuul重试功能

--- Cloud-Service2/src/main/java/com/gk/cloud/service/controller/ServiceController.java
+++ Cloud-Service2/src/main/java/com/gk/cloud/service/controller/ServiceController.java
@@ -1,12 +1,22 @@
 package com.gk.cloud.service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 
+ * @author gk
+ * 2019年3月15日 下午6:33:14
+ */
 @RestController
 public class ServiceController {
+	
+	private static Logger logger = LoggerFactory.getLogger(ServiceController.class);
 	
 	@Value("${server.port}")
 	private String serverPort;
@@ -18,6 +28,12 @@ public class ServiceController {
 	 */
 	@GetMapping("/helloCloud/{msg}")
 	public String helloCloud(@PathVariable String msg) {
+		logger.info("Request Message is {}",msg);
+		try {
+			Thread.sleep(50000);
+		} catch (Exception e) {
+			logger.error("Time out error,{}",e);
+		}
 		return String.format("Hello Spring Cloud, your message is %s from port: %s", msg, serverPort);
 	}
 	
