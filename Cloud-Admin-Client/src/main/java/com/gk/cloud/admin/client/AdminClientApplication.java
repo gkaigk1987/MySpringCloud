8388db12b730a8a382a0332d812d76781c318a5e
GaoKai <gkaigk1987@gmail.com>
2019-03-18 19:06:20 +0800

    关闭eureka自我保护及配置无效节点清理时间
    关闭eureka自我保护及配置无效节点清理时间

--- Cloud-Admin-Client/src/main/java/com/gk/cloud/admin/client/AdminClientApplication.java
+++ Cloud-Admin-Client/src/main/java/com/gk/cloud/admin/client/AdminClientApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * admin client 测试
+ * @author gk
+ * 2019年3月18日 下午1:02:33
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AdminClientApplication {
